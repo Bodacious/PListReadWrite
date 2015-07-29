0d1323575baea23d995fc5f1a358d35762590c81
Christian Segura <blackcofla@gmail.com>
2015-07-29 10:48:36 -0700

    New method to remove pListFile
    - Adding new method that removes pListFile from application documents directory

--- lib/plist_read_write/plist_read_write.rb
+++ lib/plist_read_write/plist_read_write.rb
@@ -1,12 +1,12 @@
 module PListReadWrite
-  
+
   # Custom error class used when trying to access a plist file that doesn't exist.
   class NoFileError < StandardError; end
 
   # Check if a plist file exists in either the main bundle or the documents dir.
   #
   # fileName - The name of the plist file to return, minus the .plist extension
-  # directory - A Symbol named either :documentsDir or :mainBundle depending on where you'd like 
+  # directory - A Symbol named either :documentsDir or :mainBundle depending on where you'd like
   #             to search (defaults: :documentsDir)
   #
   # Returns either True or False
@@ -22,11 +22,11 @@ module PListReadWrite
   end
 
   # Returns the path to the plist file named fileName as a String
-  # 
+  #
   # fileName - The name of the plist file to return, minus the .plist extension
-  # 
+  #
   # Examples
-  # 
+  #
   #   PListRW.plistPath('awesome')
   #   # => "/Users/Bodacious/Library/Application Support/iPhone Simulator/6.0/Applications/5A54505B-495E-4744-B8F4-DB0727FD161D/Documents/awesome.plist"
   def self.plistPath(fileName)
@@ -34,18 +34,18 @@ module PListReadWrite
   end
 
   # Update the plist file with the contents of the object passed
-  # 
+  #
   # fileName - The name of the plist file to return, minus the .plist extension
   # object - A Hash or Array with the contents of the Plist file
-  # 
+  #
   # Examples
-  #   
+  #
   #   @array = [{id: 1, name: "one"}, {id: 2, name: "two"}]
   #   PListRW.updatePlistFileWithObject('awesome', @array)
   #
   #   @hash = {1 => "one", 2 => "two"}
   #   PListRW.updatePlistFileWithObject('awesome', @hash)
-  # 
+  #
   # Raises NoFileError if the plist file doesn't exist
   def self.updatePlistFileWithObject(fileName, object)
     raise ArgumentError, "Expected object to be an instance of Array or Hash, was #{object.class}" unless object.is_a?(Array) || object.is_a?(Hash)
@@ -78,12 +78,19 @@ module PListReadWrite
     toPath       = plistPath(fileName)
     NSFileManager.defaultManager.copyItemAtPath(templatePath, toPath: toPath, error: nil)
   end
-  
+
+  # Removes the template <fileName>.plist file from application's documents directory
+  # fileName - The name of the plist file to return, minus the .plist extension
+  def self.removePListFile(fileName)
+    path = plistPath(fileName)
+    NSFileManager.defaultManager.removeItemAtPath(path, error: nil)
+  end
+
   private
-  
+
   # Private: Helper method to access the application's documents directory
   def self.documentsDir
     @@documentsDir ||= NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, true).first
   end
-  
+
 end
